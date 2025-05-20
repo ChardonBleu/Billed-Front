48b2f8a97c42f1e426cbdf884c9eb1724da661d9
ChardonBleu <f.rondi@orange.fr>
2025-05-20 16:52:05 +0200

    🔧 config: add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    ignores: [ "src/__tests__/*", "src/__mocks__/*", "coverage/*", "setup-jest.js", "block-navigation.js" ],
+    rules: {
+			"no-undef": "warn",
+      "no-unused-vars": "warn"
+		},
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+]);
