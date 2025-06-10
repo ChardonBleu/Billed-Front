3632ae7ff0d0744e929da89ceb18ff5bc415b857
Fabienne RONDI <73584139+ChardonBleu@users.noreply.github.com>
2025-06-10 11:38:31 +0200

    🧪 test: add test for getBills (#10)

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -54,6 +54,34 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+    test("Then getBills method return store bills", async () => {
+      const newBills = new Bills({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const newFrenchBills = await newBills.getBills();
+      expect(newFrenchBills[0]).toEqual(
+        {
+          id: '47qAXb6fIm2zOKkLzMro',
+          vat: '80',
+          fileUrl: 'https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a',
+          status: 'En attente',
+          type: 'Hôtel et logement',
+          commentary: 'séminaire billed',
+          name: 'encore',
+          fileName: 'preview-facture-free-201801-pdf-1.jpg',
+          date: '2004-04-04',
+          amount: 400,
+          commentAdmin: 'ok',
+          email: 'a@a',
+          pct: 20
+        }
+      )
+      expect(newFrenchBills.length).toBe(4)
+    });
     test("Then bills status should appear in French for each bill", async () => {
       const newBills = new Bills({
         document,
